bd75d5253e6a18b07620d4d98cba312863eb62fd
nhimdom200292 <kiemda02@gmail.com>
2021-11-18 09:09:24 +0700

    create new class login

---
+++ src/main/java/com/group4/model/login/Login.java
@@ -0,0 +1,14 @@
+package com.group4.model.login;
+
+import com.group4.model.account.Account;
+
+import java.time.LocalDate;
+
+public class Login {
+    private Account account;
+    private LocalDate login;
+    private LocalDate logout;
+    private Boolean status;
+
+
+}
